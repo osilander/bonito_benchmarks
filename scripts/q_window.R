2cdb28cc2e6053c14e3878544d2b9fb5405dd281
osilander <olinsilander@gmail.com>
2021-04-15 19:15:59 +1200

    some comments

--- scripts/q_window.R
+++ scripts/q_window.R
@@ -12,49 +12,65 @@ setwd(here())
 steps <- c(1e5, 2.5e5, 4e5)
 steps.lit <- c("100Kbp", "250Kbp", "400Kbp")
 
-
+# a buncch of manipulations to simplify the filenames
 files <- dir("./results", pattern=plot.pattern)
-medaka.files <- 
 names <- gsub(".snps","",files)
 names <- gsub("-","\n",names)
 names <- gsub("K12_","",names)
 names <- gsub("guppy_4.0","g4.0",names)
 names <- gsub("bonito_0.3","b0.3",names)
-cat(files, "\n")
+# print out just to check
 cat(names,"\n")
 for (s in 1:length(steps)) {
 	wins <- seq(1,4.8e6,by=steps[s])
+	
+	# we skip the last window as it is not full length
 	quals <- matrix(nrow=5e6/steps[s]-2,ncol=length(files))
 	colnames(quals) <- names
 	med.quals <- vector()
+
+	# loop over all files
 	for(f in 1:length(files)) {
 		input <- files[f]
-		cat(input,"\n")
-		
 		d <- read.table(file=paste("results/",input,sep=""))	
 		q.win <- vector()
-		
+
+		# again we skip the last window b/c not obvious 
+		# how to caluclate quality
 		for (i in 1:(length(wins)-2)) {
 			snps <- subset(d, d[,1] >= wins[i] & d[,1] < wins[i+1])
-			#q <- max(dim(snps)[1],0, na.rm=TRUE)
+			
+			# it's not obvious how too caluclate this
+			# if there are no errors
 			q <- max(1,dim(snps)[1])
-			#cat(q,"\n")
 			quals[i,f] <- -log10(q/steps[s])*10
 		}
 		med.quals[f] <- median(quals[,f])
 	}
-	o.quals <- order(med.quals)
+
+	# we could order by median but perhaps not
 	quals.df <- as.data.frame(quals)
+	
+	# make the matrix long and use factors (for plotting sake)
 	quals.long <- gather(quals.df, key="Assembler",value="qscore")
 
+	# make the figures directory if needed
 	ifelse(!dir.exists(file.path("./", "figures")), dir.create(file.path("./", "figures")), FALSE)
+	
+	# open a png
 	png.name <- paste("figures/quals_beeswarm_",args[1],"_",steps.lit[s],".png",sep="")
 	png(file=png.name, width=100*length(files),height=480)
+	
+	# change some defaults
 	par(las=1)
 	par(mar=c(8,5,2,1))
+
+	# get the beeswarm plot
 	beeswarm(qscore ~ Assembler, data = quals.long, 
 	  log = FALSE, pch = 16, col = rainbow(8),
 	  main = (paste("qscore -",steps.lit[s], "window", sep=" ")), cex.main=1.2, xlab="basecaller-assembler-polisher")
+	
+	# add a few lines
 	abline(h=seq(30,55,by=5),lty=2,lwd=0.5,col="grey",font.main=1)
 	dev.off()
 }
